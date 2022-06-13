70ec252dde6ec6fb56a16f72a06d48e29ba8a159
Павел Касапов <p.v.kasapov@gmail.com>
2022-06-13 22:13:45 +0300

    - First movement try;

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D _rigidbody;
+    private PlayerControls _controls;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _controls = new PlayerControls();
+        _controls.CommonMovement.Jump.performed += Jump;
+        _controls.CommonMovement.HorisontalMovement.performed += (ctx => Move(ctx.ReadValue<float>()));
     }
 
     // Update is called once per frame
-    void Update()
+    void Move(float value)
     {
-        
+        Debug.Log(value);
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(ctx);
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 1f);
     }
 }
