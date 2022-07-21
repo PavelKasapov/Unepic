d8d6b8fec2557cac71d4155d8e345d1cf17a067e
Павел Касапов <p.v.kasapov@gmail.com>
2022-07-22 01:48:49 +0300

    Улучшил систему передвижения

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,31 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Timeline;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D _rigidbody;
+    private const float GroundCheckLenght = 0.1f;
+    
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private new Collider2D collider;
+    [SerializeField] private LayerMask whatIsGround = 2; 
+    [SerializeField] private Transform groundLevelPoint;
     [SerializeField] private float speed = 2; 
     
     private PlayerControls _controls;
-    
+    private int _faceDirection = 1;
+    private float _moveValue = 0f;
+    private Coroutine _moveCoroutine;
+    private Coroutine _groundCheckCoroutine;
+
     private void Awake()
     {
         _controls = new PlayerControls();
+
+        _controls.CommonMovement.HorisontalMovement.performed += ctx => Move(ctx.ReadValue<float>());
+        _controls.CommonMovement.HorisontalMovement.canceled += ctx => Move(ctx.ReadValue<float>());
+        _controls.CommonMovement.Jump.performed += ctx => Jump(ctx.ReadValue<float>());
     }
 
     private void OnEnable()
@@ -23,28 +38,79 @@ public class PlayerMovement : MonoBehaviour
         _controls.Disable();
     }
 
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         Jump(_controls.CommonMovement.Jump.ReadValue<float>());
         Move(_controls.CommonMovement.HorisontalMovement.ReadValue<float>());
-    }
+    }*/
 
-    // Update is called once per frame
     void Move(float value)
     {
+        Debug.Log($"!! Move {value}");
         if (value != 0f)
         {
-            var localScale = transform.localScale;
-            localScale = new Vector3(Mathf.Sign(value) * Mathf.Abs(localScale.x), localScale.y, localScale.z);
-            transform.localScale = localScale;
+            if (Math.Sign(value) != _faceDirection)
+            {
+                _faceDirection *= -1;
+                var thisTransform = transform;
+                var localScale = thisTransform.localScale;
+                localScale.x *= -1;
+                thisTransform.localScale = localScale;
+            }
+        }
+        
+        _moveValue = value;
+        
+        if (IsGrounded())
+        {
+            _moveCoroutine ??= StartCoroutine(MoveRoutine());
+        }
+    }
+
+    IEnumerator MoveRoutine()
+    {
+        var moveValue = _moveValue;
+        while (_moveValue != 0f || _groundCheckCoroutine != null)
+        {
+            if ( moveValue != _moveValue && _groundCheckCoroutine == null)
+            {
+                moveValue = _moveValue;
+            }
+            Debug.Log($"!! {_moveValue} {_groundCheckCoroutine != null}");
+            rigidbody.velocity = new Vector2(moveValue * speed, rigidbody.velocity.y);
+            yield return new WaitForFixedUpdate();
+        }
+        
+        rigidbody.velocity = new Vector2(_moveValue, rigidbody.velocity.y);
+        _moveCoroutine = null;
+    }
+    
+    IEnumerator DelayedMoveRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        
+        while (!IsGrounded())
+        {
+            yield return new WaitForFixedUpdate();
         }
 
-        _rigidbody.velocity = new Vector2(value * speed, _rigidbody.velocity.y);
+        Move(_moveValue);
+
+        _groundCheckCoroutine = null;
     }
 
     void Jump(float value)
     {
-        if (value > 0)
-            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, value * 5);
+        Debug.Log($"!! Jump {value}");
+        if (IsGrounded())
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, value * 3);
+            _groundCheckCoroutine ??= StartCoroutine(DelayedMoveRoutine());
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.Raycast(groundLevelPoint.position, Vector2.down, GroundCheckLenght, whatIsGround).collider != null;
     }
 }
